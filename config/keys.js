d11df7572a9be07824eed2220c0e06e625b512ce
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2020-12-19 13:50:10 +0530

    We do not need this

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-dbPassword = 'mongodb+srv://admin:'+ encodeURIComponent('vHwH175KqfyndrOD') + '@cluster0.brwhf.mongodb.net/test?retryWrites=true';
-
-module.exports = {
-    mongoURI: dbPassword
-};

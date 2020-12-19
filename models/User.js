1a2f8270554552d39de4688e6de875c661bebb08
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2020-12-19 13:36:42 +0530

    for new feature

--- models/User.js
+++ models/User.js
@@ -49,6 +49,10 @@ const UserSchema = new mongoose.Schema({
         type: Boolean,
         required: false
     },
+    token: {
+        type: String,
+        required: false
+    },
     date: {
         type: Date,
         required: false

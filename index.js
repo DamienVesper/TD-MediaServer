ae3353aea11612365c2bf2daee8306781490b125
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2020-12-28 19:27:56 +0530

    dash and hls are back

--- index.js
+++ index.js
@@ -37,6 +37,10 @@ const config = {
                 app: 'live',
                 mp4: true,
                 mp4Flags: '[movflags=frag_keyframe+empty_moov]',
+                hls: false,
+                hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+                dash: true,
+                dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
             }
         ]
     }

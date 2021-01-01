c07d010ee2597df2d5fcd1d8b515b2e6fc6ac84e
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2021-01-01 15:07:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,18 +27,6 @@ const config = {
         api_user: 'admin',
         api_pass: 'loltdtv2021'
     },
-    trans: {
-        ffmpeg: '/usr/bin/ffmpeg',
-        tasks: [
-            {
-                app: 'live',
-                hls: true,
-                hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-                dash: true,
-                dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
-            }
-        ]
-    },
 };
 
 var nms = new NodeMediaServer(config)

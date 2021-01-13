54286e0b8b9dfdd087c16c00d68c1d983472131b
Rajeev Kapoor <44755991+RajeevKapoor@users.noreply.github.com>
2021-01-13 17:20:46 +0530

    Testing Notifications

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const NodeMediaServer = require('node-media-server');
 const axios = require('axios');
+const User = require('./models/User')
 
 const config = {
     server: {
@@ -31,7 +32,18 @@ const config = {
 
 var nms = new NodeMediaServer(config)
 nms.run();
-
+let transporter = nodemailer.createTransport({
+    host: "smtp.throwdown.tv",
+    port: 25,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: "notifications@throwdown.tv", // generated ethereal user
+        pass: "Dankmeme2000", // generated ethereal password
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
 nms.on('prePublish', async (id, StreamPath, args) => {
     let stream_key = getStreamKeyFromStreamPath(StreamPath);
     console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
@@ -44,6 +56,24 @@ nms.on('prePublish', async (id, StreamPath, args) => {
                 console.log("Stream key does not exist " + stream_key)
             } else {
                 console.log("Stream key does exist " + stream_key)
+                User.findOne({ stream_key: stream_key }).then(useraccount => {
+                    useraccount.followers.forEach(user => {
+                        User.findOne({username: user}).then(useracc => {
+                            let message = {
+                                from: "Throwdown TV Notifications <notifications@throwdown.tv>",
+                                to: useracc.email,
+                                subject: user + " is now Live!",
+                                text: `${useraccount.username} went live with the title "${useraccount.stream_title}". Watch here: https://throwdown.tv/${useraccount.username}` ,
+                            };
+                            transporter.sendMail(message, (error, info) => {
+                                if (error) {
+                                    return console.log(error);
+                                }
+                                console.log('Message sent: %s', info.messageId);
+                            });
+                        })
+                    })
+                })
             }
         })
         .catch(function (error) {
---
+++ models/User.js
@@ -0,0 +1,99 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    stream_key: {
+        type: String,
+    },
+    chat_key: {
+        type: String,
+    },
+    stream_title: {
+        type: String,
+        default: "My cool stream :)"
+    },
+    stream_description: {
+        type: String,
+        default: "Description of my cool stream :)"
+    },
+    avatar_url: {
+        type: String,
+        default: "https://cdn.discordapp.com/attachments/736368923590525039/789419292214820894/defaulltpfp.png"
+    },
+    email_verification_key: {
+        type: String,
+    },
+    verification_status: {
+        type: Boolean,
+        default: false
+    },
+    donation_link: {
+        type: String,
+        default: "/streams/donate"
+    },
+    can_stream: {
+        type: Boolean,
+        default: false
+    },
+    banned: {
+        type: Boolean,
+        default: false
+    },
+    banreason: {
+        type: String,
+        default: "TOS Violation"
+    },
+    following: {
+        type: Array,
+    },
+    followers: {
+        type: Array,
+    },
+    live_viewers: {
+        default: 0
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    discordID: {
+        type: String,
+        default: "YOUR_DISCORD_ID_HERE"
+    },
+    isVip: {
+        type: Boolean,
+        default: false
+    },
+    isStaff: {
+        type: Boolean,
+        default: false
+    },
+    moderators: {
+        type: Array
+    },
+    ips: {
+        type: Array
+    },
+    banlist: {
+        type: Array
+    },
+    banned_words: {
+        type: Array
+    }
+
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;

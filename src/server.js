d018e30afe963bef7687c6ef2b8f020a16ab37d3
Damien Vesper <ldamienvesper@gmail.com>
2021-01-30 23:56:57 -0600

    ok no we're going back

--- src/server.js
+++ src/server.js
@@ -10,53 +10,32 @@ const server = new NodeMediaServer(rtmpConfig);
 // Log errors in a different color.
 process.on(`uncaughtException`, err => log(`red`, err.stack));
 
-const nms = new NodeMediaServer(config);
-nms.run();
-nms.on(`prePublish`, async (id, StreamPath, args) => {
-    const streamKey = getStreamKeyFromStreamPath(StreamPath);
-    console.log(`[NodeEvent on prePublish]`, `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-    await axios.get(`https://throwdown.tv/api/streamkey/${streamKey}`)
-        .then(async (response) => {
-            // Check if it works
-            if (!response.data.canstream) {
-                const session = nms.getSession(id);
-                session.reject();
-                console.log(`Stream key does not exist ${streamKey}`);
-            } else {
-                console.log(`Stream key does exist ${streamKey}`);
-                await axios.get(`https://throwdown.tv/api/send_notification_email/${process.env.NOTIFICATION_API_KEY}/${response.data.username}`);
-            }
-        })
-        .catch((error) => {
-            // handle error
-            console.log(error);
-        });
+// Start the server.
+server.run();
+
+server.on(`prePublish`, async (id, streamer, streamKey) => {
+    if (!id || !streamer || !streamKey) return;
+
+    const session = server.getSession(id);
+    axios.get(`https://${config.webfrontName}/api/verifyStreamer?name=${streamer}&key=${streamKey}`).then(res => {
+        const data = res.data;
+
+        // If the person cannot stream or the credentials were not verified by the server, then reject the session request.
+        if (!data.canStream || !data.verified) {
+            log(`cyan`, `User attempted to stream with invalid stream key.`);
+            return session.reject();
+        }
+        else log(`magenta`, `User established to stream with valid stream key.`);
+    }).catch(() => {
+        log(`red`, `Failed to verify streamer.`);
+        session.reject();
+    });
 });
 
-setInterval(() => {
-    axios.get(`https://throwdown.tv/api/streamkey/${streamKey}`)
-        .then((response) => {
-            // Check if it works
-            if (response.data.canstream === false) {
-                const session = nms.getSession(id);
-                session.reject();
-            }
-        })
-        .catch((error) => {
-            // handle error
-            console.log(error);
-        });
-}, 10000);
-
-nms.on(`donePlay`, (id, StreamPath, args) => {
-    const session = nms.getSession(id);
+server.on(`donePlay`, id => {
+    const session = server.getSession(id);
     session.reject();
 });
-
-const getStreamKeyFromStreamPath = (path) => {
-    const parts = path.split(`/`);
-    return parts[parts.length - 1];
-};
 
 // Export server.
 module.exports = server;

d5c5d48a9dbe6c52d36e80633aaebd483806d6e1
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-02-13 18:11:34 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const axios = require(`axios`);
 
 // Landing page.
 router.get(`/stream/:streamer`, async (req, res) => {
-    const streamer = req.params.streamer;
+    const streamer = req.params.streamer.toLowerCase();
     const getStreamKey = await axios.get(`https://${config.webfrontName}/api/rtmp-api/${streamer}/${process.env.FRONTEND_API_KEY}`);
     const getStreamVideo = await axios.get(`http://localhost:8951/api/streams/live/${user.stream_key}`, { auth: { username: `admin`, password: process.env.RTMP_API_PASSWORD } });
     if (getStreamKey.data.errors.length === 0) return res.json({ errors: `User does not exist` });

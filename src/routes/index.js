0b4117b8d7a7ceb8760dd870b3e991d08dc39aef
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-02-13 18:23:27 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const config = require(`../../config/config.js`);
 const axios = require(`axios`);
 
+// Index page.
+router.get(`/`, async (req, res) => { res.redirect(`https://${config.webfrontName}`) })
+
 // Landing page.
 router.get(`/stream/:streamer`, async (req, res) => {
     const streamer = req.params.streamer.toLowerCase();
     const getStreamKey = await axios.get(`https://${config.webfrontName}/api/rtmp-api/${streamer}/${process.env.FRONTEND_API_KEY}`);
-    const getStreamVideo = await axios.get(`http://localhost:8951/api/streams/live/${user.stream_key}`, { auth: { username: `admin`, password: process.env.RTMP_API_PASSWORD } });
+    const getStreamVideo = await axios.get(`http://localhost:8951/api/streams/live/${getStreamVideo.streamkey}`, { auth: { username: `admin`, password: process.env.RTMP_API_PASSWORD } });
     if (getStreamKey.data.errors.length === 0) return res.json({ errors: `User does not exist` });
     if (!getStreamVideo.data.isLive) return res.json({ errors: `Streamer not live` });
     res.send(`IS LIVE`);

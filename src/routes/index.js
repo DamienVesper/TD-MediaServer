a61738fddf15e528a5321050ff93c67efb443b7b
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-02-13 19:04:25 +0530

    uh

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require(`express`);
 const router = express.Router();
 const config = require(`../../config/config.js`);
 const axios = require(`axios`);
-const http = require(`http`);
+const request = require(`request`);
 
 // Index page.
 router.get(`/`, async (req, res) => { res.redirect(`https://${config.webfrontName}`); });
@@ -12,14 +12,8 @@ router.get(`/stream/:streamer`, async (req, res) => {
     const streamer = req.params.streamer.toLowerCase();
     const getStreamKey = await axios.get(`https://${config.webfrontName}/api/rtmp-api/${streamer}/${process.env.FRONTEND_API_KEY}`);
     if (getStreamKey.data.errors) return res.json({ errors: `User does not exist` });
-    const externalReq = http.request({
-        hostname: `localhost:8946`,
-        path: `/live/${getStreamKey.streamkey}.flv`
-    }, (externalRes) => {
-        res.setHeader(`content-disposition`, `attachment; filename=index.flv`);
-        externalRes.pipe(res);
-    });
-    externalReq.end();
+    const url = `http://localhost:8946/live/${getStreamKey.streamkey}.flv`;
+    request.get(url).pipe(res);
 });
 
 module.exports = router;

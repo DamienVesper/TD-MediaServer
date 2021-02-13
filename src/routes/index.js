e4eed7606cea43f469300217aabc5c59523257f7
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-02-13 19:21:47 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,8 +12,14 @@ router.get(`/stream/:streamer`, async (req, res) => {
     const streamer = req.params.streamer.toLowerCase();
     const getStreamKey = await axios.get(`https://${config.webfrontName}/api/rtmp-api/${streamer}/${process.env.FRONTEND_API_KEY}`);
     if (getStreamKey.data.errors) return res.json({ errors: `User does not exist` });
-    const url = `http://localhost:8946/live/${getStreamKey.data.streamkey}.flv`;
-    request.get(url).pipe(res);
+    const externalReq = http.request({
+        host: `localhost:8946`,
+        path: `/live/${getStreamKey.data.streamkey}.flv`
+    }, (externalRes) => {
+        res.setHeader(`content-disposition`, `attachment; filename=index.flv`);
+        externalRes.pipe(res);
+    });
+    externalReq.end();
 });
 
 module.exports = router;

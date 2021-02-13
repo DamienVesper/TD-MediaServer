36f96788d200b21e925dc16ad0d7a0b8e2e547f3
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-02-13 19:12:30 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,6 +12,7 @@ router.get(`/stream/:streamer`, async (req, res) => {
     const streamer = req.params.streamer.toLowerCase();
     const getStreamKey = await axios.get(`https://${config.webfrontName}/api/rtmp-api/${streamer}/${process.env.FRONTEND_API_KEY}`);
     if (getStreamKey.data.errors) return res.json({ errors: `User does not exist` });
+    console.log(getStreamKey.streamkey);
     const url = `http://localhost:8946/live/${getStreamKey.streamkey}.flv`;
     request.get(url).pipe(res);
 });

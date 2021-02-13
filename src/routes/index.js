d510eca843340e2fca2207ff9a8aacaf2c9d7e96
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-02-13 18:58:32 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,18 +2,24 @@ const express = require(`express`);
 const router = express.Router();
 const config = require(`../../config/config.js`);
 const axios = require(`axios`);
+const http = require(`http`);
 
 // Index page.
-router.get(`/`, async (req, res) => { res.redirect(`https://${config.webfrontName}`) })
+router.get(`/`, async (req, res) => { res.redirect(`https://${config.webfrontName}`); });
 
 // Landing page.
 router.get(`/stream/:streamer`, async (req, res) => {
     const streamer = req.params.streamer.toLowerCase();
     const getStreamKey = await axios.get(`https://${config.webfrontName}/api/rtmp-api/${streamer}/${process.env.FRONTEND_API_KEY}`);
-    const getStreamVideo = await axios.get(`http://localhost:8946/api/streams/live/${getStreamKey.streamkey}`, { auth: { username: `admin`, password: process.env.RTMP_API_PASSWORD } });
     if (getStreamKey.data.errors) return res.json({ errors: `User does not exist` });
-    if (getStreamVideo.data.isLive === false) return res.json({ errors: `Streamer not live` });
-    res.send(`IS LIVE`);
+    const externalReq = http.request({
+        hostname: `localhost:8946`,
+        path: `/live/${getStreamKey.streamkey}.flv`
+    }, (externalRes) => {
+        res.setHeader(`content-disposition`, `attachment; filename=index.flv`);
+        externalRes.pipe(res);
+    });
+    externalReq.end();
 });
 
 module.exports = router;

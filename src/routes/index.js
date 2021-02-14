471c9a53724abe9f091941ae67af5f3372495666
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-02-14 12:49:21 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,6 +14,8 @@ router.get(`/stream/:streamer`, async (req, res) => {
     const getStreamKey = await axios.get(`${config.webPath}/api/rtmp-api/${streamer}/${process.env.FRONTEND_API_KEY}`);
     if (getStreamKey.data.errors) return res.json({ errors: `User does not exist` });
 
+    res.setHeader(`content-disposition`, `attachment; filename=index.flv`);
+
     http.get(`http://localhost:${config.ports.nmsHTTP}/live/${getStreamKey.data.streamkey}.flv`, response => response.pipe(res));
 });
 

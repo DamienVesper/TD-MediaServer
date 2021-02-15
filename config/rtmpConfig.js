6a40a81cf6e5f7805310771d50a1d40c2f93166e
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-02-15 20:03:45 +0530

    Update rtmpConfig.js

--- config/rtmpConfig.js
+++ config/rtmpConfig.js
@@ -14,12 +14,25 @@ const rtmpConfig = {
     },
     http: {
         port: config.ports.nmsHTTP,
+        mediaroot: `./media`,
         allow_origin: `*`
     },
     auth: {
         api: false,
         api_user: `admin`,
         api_pass: process.env.RTMP_API_PASSWORD
+    },
+    trans: {
+        ffmpeg: `/usr/bin/ffmpeg`,
+        tasks: [
+            {
+                app: `live`,
+                hls: true,
+                hlsFlags: `[hls_time=2:hls_list_size=3:hls_flags=delete_segments]`,
+                dash: true,
+                dashFlags: `[f=dash:window_size=3:extra_window_size=5]`
+            }
+        ]
     }
 };
 

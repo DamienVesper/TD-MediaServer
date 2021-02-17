d4632164ac2f0a2ca68d5383fdc08ee13f753e57
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-02-17 16:18:32 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -89,7 +89,7 @@ server.on(`donePublish`, (id, streamPath, args) => {
             log(`red`, res.data.errors);
         }
         else {
-            fs.rmSync(`${mediadirectory}/${streamKey}.png`);
+            fs.rmSync(path.join(__dirname, `../media/${streamKey}.png`));
             streams.splice(streams.indexOf(streamerData), 1);
             log(`magenta`, `User Disconnected.`);
         }

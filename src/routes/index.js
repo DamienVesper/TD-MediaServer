36c65aca02eff259ae0951afb5dd99dd979b3c98
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-03-26 16:40:54 +0530

    Kek

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const config = require(`../../config/config.js`);
 const axios = require(`axios`);
 const path = require(`path`);
+const fs = require(`fs`);
 const http = require(`http`);
 
 // Index page.
@@ -30,7 +31,7 @@ router.get(`/stream_source/:streamer`, async (req, res) => {
     http.get(`http://localhost:${config.ports.nmsHTTP}/live/${getStreamKey.data.streamkey}.flv`, response => response.pipe(res));
 });
 
-// 1080p
+// HLS
 router.get(`/stream_hls/:streamer`, async (req, res) => {
     const streamer = req.params.streamer.toLowerCase();
 
@@ -39,7 +40,16 @@ router.get(`/stream_hls/:streamer`, async (req, res) => {
 
     res.setHeader(`content-disposition`, `attachment; filename=index.m3u8`);
 
-    http.get(`http://localhost:${config.ports.nmsHTTP}/live/${getStreamKey.data.streamkey}/index.m3u8`, response => response.pipe(res));
+    const filePath = `../../media/${req.url}`;
+
+    fs.readFile(filePath, (error, content) => {
+        res.writeHead(200, { 'Access-Control-Allow-Origin': `*` });
+        if (error) {
+            res.send(`404`);
+        } else {
+            res.end(content, `utf-8`);
+        }
+    });
 });
 
 /**

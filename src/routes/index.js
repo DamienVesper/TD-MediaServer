0a8b259d9e0d6c26cff2e226bc770a535014de86
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-05-12 16:18:56 +0530

    Testing this out

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const config = require(`../../config/config.js`);
 const axios = require(`axios`);
 const path = require(`path`);
 const http = require(`http`);
+const fs = require(`fs`);
 
 // Index page.
 router.get(`/`, async (req, res) => res.redirect(config.webPath));
@@ -18,7 +19,7 @@ router.get(`/thumbnail/:streamer`, async (req, res) => {
     res.sendFile(path.join(__dirname, `../../media/${getStreamKey.data.streamkey}.png`));
 });
 
-// Stream Feed.
+// FLV Feed.
 router.get(`/stream_source/:streamer`, async (req, res) => {
     const streamer = req.params.streamer.toLowerCase();
 
@@ -30,65 +31,40 @@ router.get(`/stream_source/:streamer`, async (req, res) => {
     http.get(`http://localhost:${config.ports.nmsHTTP}/live/${getStreamKey.data.streamkey}.flv`, response => response.pipe(res));
 });
 
-/**
-// HLS
-router.get(`/stream_hls/:streamer`, async (req, res) => {
+// HLS Feed.
+router.get(`/stream_hlse/:streamer`, async (req, res) => {
     const streamer = req.params.streamer.toLowerCase();
 
     const getStreamKey = await axios.get(`https://${config.webfrontName}/api/rtmp-api/${streamer}/${process.env.FRONTEND_API_KEY}`);
     if (getStreamKey.data.errors) return res.json({ errors: `User does not exist` });
 
-    res.setHeader(`content-disposition`, `attachment; filename=index.m3u8`);
+    res.setHeader(`content-disposition`, `attachment; filename=index.flv`);
 
-    const filePath = `../../media/${req.url}`;
+    const filePath = `http://localhost:${config.ports.nmsHTTP}/live/${getStreamKey.data.streamkey}`;
 
-    fs.readFile(filePath, (error, content) => {
-        res.writeHead(200, { 'Access-Control-Allow-Origin': `*` });
-        if (error) {
-            res.end();
-        } else {
-            res.end(content, `utf-8`);
+    fs.stat(filePath, (err, stats) => {
+        if (!stats || err) {
+            res.status(404).end();
+        }
+        else {
+            let fileExt = filePath.slice(filePath.lastIndexOf(`.`));
+            fileExt = fileExt && fileExt.toLowerCase() || fileExt;
+
+            switch (fileExt) {
+                case `.m3u8`:
+                    res.status(200).set(`Content-Type`, `application/vnd.apple.mpegurl`);
+                    fs.createReadStream(filePath).pipe(res);
+                    break;
+                case `.ts`:
+                    res.status(200).set(`Content-Type`, `video/MP2T`);
+                    fs.createReadStream(filePath).pipe(res);
+                    break;
+                default:
+                    res.status(400).send(`Unexpected file type ${fileExt}`);
+            }
         }
     });
 });
-
-// 720
-router.get(`/stream_720/:streamer`, async (req, res) => {
-    const streamer = req.params.streamer.toLowerCase();
-
-    const getStreamKey = await axios.get(`https://${config.webfrontName}/api/rtmp-api/${streamer}/${process.env.FRONTEND_API_KEY}`);
-    if (getStreamKey.data.errors) return res.json({ errors: `User does not exist` });
-
-    res.setHeader(`content-disposition`, `attachment; filename=index.flv`);
-
-    http.get(`http://localhost:${config.ports.nmsHTTP}/live/${getStreamKey.data.streamkey}_720.flv`, response => response.pipe(res));
-});
-
-// 480
-router.get(`/stream_480/:streamer`, async (req, res) => {
-    const streamer = req.params.streamer.toLowerCase();
-
-    const getStreamKey = await axios.get(`https://${config.webfrontName}/api/rtmp-api/${streamer}/${process.env.FRONTEND_API_KEY}`);
-    if (getStreamKey.data.errors) return res.json({ errors: `User does not exist` });
-
-    res.setHeader(`content-disposition`, `attachment; filename=index.flv`);
-
-    http.get(`http://localhost:${config.ports.nmsHTTP}/live/${getStreamKey.data.streamkey}_480.flv`, response => response.pipe(res));
-});
-
-// 360
-router.get(`/stream_360/:streamer`, async (req, res) => {
-    const streamer = req.params.streamer.toLowerCase();
-
-    const getStreamKey = await axios.get(`https://${config.webfrontName}/api/rtmp-api/${streamer}/${process.env.FRONTEND_API_KEY}`);
-    if (getStreamKey.data.errors) return res.json({ errors: `User does not exist` });
-
-    res.setHeader(`content-disposition`, `attachment; filename=index.flv`);
-
-    http.get(`http://localhost:${config.ports.nmsHTTP}/live/${getStreamKey.data.streamkey}_360.flv`, response => response.pipe(res));
-});
-
-*/
 
 // API
 router.get(`/api/:streamer`, async (req, res) => {

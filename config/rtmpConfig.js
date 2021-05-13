6c7d6fd76023057329b0350bcf269fe89e5181a7
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-05-13 10:23:36 +0530

    Update rtmpConfig.js

--- config/rtmpConfig.js
+++ config/rtmpConfig.js
@@ -30,12 +30,6 @@ const rtmpConfig = {
                 model: [
                     {
                         ab: `64k`,
-                        vb: `700k`,
-                        vs: `1280x720`,
-                        vf: `30`
-                    },
-                    {
-                        ab: `64k`,
                         vb: `500k`,
                         vs: `854x480`,
                         vf: `30`

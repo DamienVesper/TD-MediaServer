8d7a8e641fb62c94df74fc34eca30ea4d916c8c8
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-05-13 10:24:11 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -32,19 +32,7 @@ router.get(`/stream_flv_source/:streamer`, async (req, res) => {
     http.get(`http://localhost:${config.ports.nmsHTTP}/live/${getStreamKey.data.streamkey}.flv`, response => response.pipe(res));
 });
 
-// FLV 720p Feed.
-router.get(`/stream_flv_720/:streamer`, async (req, res) => {
-    const streamer = req.params.streamer.toLowerCase();
-
-    const getStreamKey = await axios.get(`https://${config.webfrontName}/api/rtmp-api/${streamer}/${process.env.FRONTEND_API_KEY}`);
-    if (getStreamKey.data.errors) return res.json({ errors: `User does not exist` });
-
-    res.setHeader(`content-disposition`, `attachment; filename=index.flv`);
-
-    http.get(`http://localhost:${config.ports.nmsHTTP}/live/${getStreamKey.data.streamkey}_720.flv`, response => response.pipe(res));
-});
-
-// FLV Source Feed.
+// 480 Source Feed.
 router.get(`/stream_flv_480/:streamer`, async (req, res) => {
     const streamer = req.params.streamer.toLowerCase();
 

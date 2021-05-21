42399d32220e7b90ad54a5c73717400d2379cef9
Madhav Kothandaraman <44755991+MadhavWasTaken@users.noreply.github.com>
2021-05-21 21:49:56 +0530

    Update webfront.js

--- src/webfront.js
+++ src/webfront.js
@@ -19,9 +19,6 @@ app.use(cors({ origin: `*` }));
 const compression = require(`compression`);
 const bodyParser = require(`body-parser`);
 
-// Static Media Folder
-app.use(express.static(`media`));
-
 app.use(compression());
 app.use(bodyParser.json({
     limit: `50mb`

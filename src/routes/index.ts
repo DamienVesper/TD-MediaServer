6aa015c0f6b7b674549270fa5d3fe299ac225709
Rajeev Kapoor <44755991+MadhavWasTaken@users.noreply.github.com>
2021-06-12 04:34:28 +0530

    Revert "I guess this works"
    This reverts commit 627c05a7b1417ffe5cb40e7f28edee109d3b11ca.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -21,15 +21,8 @@ router.get(`/thumbnail/:streamer`, async (req: Express.Request, res: Express.Res
 
     const getStreamKey = await axios.post(`${config.webfront}/api/rtmp-api`, post);
 
-    const getStreamData = await axios.get(`http://localhost:${config.ports.nms}/api/streams/live/${getStreamKey.data.streamkey}`, {
-        auth: {
-            username: `admin`,
-            password: process.env.RTMP_API_PASSWORD
-        }
-    });
-
-    if (getStreamData.data.errors) return res.json({ errors: `User does not exist` });
-    if (!getStreamData.data.isLive) return res.sendFile(path.join(__dirname, `../assets/thumbnail.png`));
+    if (getStreamKey.data.errors) return res.json({ errors: `User does not exist` });
+    if (!getStreamKey.data.isLive) return res.sendFile(path.join(__dirname, `../../assets/thumbnail.png`));
 
     res.sendFile(path.join(__dirname, `../../media/${getStreamKey.data.streamkey}.png`));
 });

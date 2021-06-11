99a1200b99aa98637a655b4c7c5c6938c8726075
Damien Vesper <ldamienvesper@gmail.com>
2021-06-11 15:45:07 -0500

    add the rtmp config

--- config/rtmpConfig.ts
+++ config/rtmpConfig.ts
@@ -1,35 +1,29 @@
-require(`dotenv`).config();
-const config = require(`./config.js`);
+import config from './config';
 
 const rtmpConfig = {
     logType: 3,
     server: {
         secret: process.env.RTMP_TOKEN
     },
+
     rtmp: {
-        port: config.ports.server,
+        port: 1935,
         chunk_size: 6000,
         gop_cache: true,
         ping: 30
     },
+
     http: {
-        port: config.ports.nmsHTTP,
+        port: config.ports.nms,
         mediaroot: `./media`,
         allow_origin: `*`
     },
+
     auth: {
-        api: false,
+        api: true,
         api_user: `admin`,
         api_pass: process.env.RTMP_API_PASSWORD
     }
 };
 
-if (config.mode === `prod`) {
-    rtmpConfig.https = {
-        port: config.ports.nmsHTTPS,
-        key: config.ssl.keyPath,
-        cert: config.ssl.certPath
-    };
-}
-
-module.exports = rtmpConfig;
+export default rtmpConfig;

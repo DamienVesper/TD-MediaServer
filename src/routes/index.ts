b2cffb4634d24f67c0734cb22edc8551d68dc6a7
Rajeev Kapoor <44755991+MadhavWasTaken@users.noreply.github.com>
2021-06-15 12:24:51 +0530

    what about this

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -21,7 +21,7 @@ router.get(`/flv/:streamer`, async (req, res) => {
 
     res.setHeader(`content-disposition`, `attachment; filename=index.flv`);
 
-    await request.get(`http://localhost:${config.ports.nms}/live/${getStreamKey.data.streamkey}.flv`).pipe(res);
+    await request.get(`http://localhost:${config.ports.nms}/live/${getStreamKey.data.streamkey}.flv`, { highWaterMark: 1024000, encoding: null }).pipe(res, { highWaterMark: 1024000 });
 });
 
 export default router;

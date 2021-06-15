a080d08b5ae2b429c3302ce2233415cd4d0900c9
Rajeev Kapoor <44755991+MadhavWasTaken@users.noreply.github.com>
2021-06-15 12:12:49 +0530

    Will this fix the mem leak

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import Express from 'express';
 
 import config from '../../config/config';
 import axios from 'axios';
-import request from 'request-promise';
+import fetch from 'node-fetch';
 
 const router: Express.Router = Express.Router();
 
@@ -10,18 +10,19 @@ const router: Express.Router = Express.Router();
 router.get(`/`, async (req: Express.Request, res: Express.Response) => res.redirect(config.webfront));
 
 // FLV Source Feed.
-router.get(`/flv/:streamer`, async (req, res) => {
+router.get(`/flv/:streamer`, (req, res) => {
     const streamer = req.params.streamer.toLowerCase();
     const post = {
         apiKey: process.env.FRONTEND_API_KEY,
         streamer: streamer
     };
-    const getStreamKey = await axios.post(`${config.webfront}/api/rtmp-api`, post);
-    if (getStreamKey.data.errors || getStreamKey.status === 404) return res.json({ errors: `User does not exist` });
-
-    res.setHeader(`content-disposition`, `attachment; filename=index.flv`);
-
-    await request.get(`http://localhost:${config.ports.nms}/live/${getStreamKey.data.streamkey}.flv`).pipe(res);
+    axios.post(`${config.webfront}/api/rtmp-api`, post).then((response) => {
+        if (response.status === 404) return res.json({ errors: `User does not exist` });
+        res.setHeader(`content-disposition`, `attachment; filename=index.flv`);
+        fetch(`http://localhost:${config.ports.nms}/live/${response.data.streamkey}.flv`).then(r => r.body).then(s => { s.pipe(res); }).catch(e => { res.status(500).send(`Error.`); });
+    }).catch((error) => {
+        res.json({ errors: error });
+    });
 });
 
 export default router;
